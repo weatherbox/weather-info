8d922c2c5beeb84437c900f36075ff9f1e54a1c5
tattii <yuta.tatti@gmail.com>
2018-12-11 04:52:40 +0900

    dispatch updated

--- jma-xml-dispatch/dispatch.js
+++ jma-xml-dispatch/dispatch.js
@@ -10,7 +10,7 @@ const atomFeed = 'http://www.data.jma.go.jp/developer/xml/feed/extra.xml';
 update();
 
 
-async function update() {
+function update() {
   const transaction = datastore.transaction();
 
   transaction
@@ -21,36 +21,53 @@ async function update() {
       const last = res[0];
       const lastId = last.lastId;
 
-      last.lastId = checkUpdated(lastId);
-      transaction.save({
-        key: lastIdKey,
-        data: last
+      checkUpdated(lastId, function(updatedLastId) {
+        console.log('updated: ' + updatedLastId);
+        last.lastId = updatedLastId;
+        transaction.save({
+          key: lastIdKey,
+          data: last
+        });
+        return transaction.commit();
       });
-      return transaction.commit();
     });
 }
 
-async function checkUpdated(lastId) { 
+function checkUpdated(lastId, done) { 
   request(atomFeed, (err, res, body) => {
     var parser = new xml2js.Parser();
     parser.parseString(body, (err, feed) => {
-      console.log(JSON.stringify(feed, null, 2));
+      let count = 0;
 
       for (var entry of feed.feed.entry){
         const id = entry.id[0];
         const title = entry.title[0];
         const xml = entry.link[0].$.href;
-        console.log(id, title, xml);
+        if (id == lastId) break;
 
-        dispatch(title, xml);
+        dispatch(id, title, xml);
+        count++;
       }
+    
+      console.log('updated ' + count + ' xmls');
+      done(feed.feed.entry[0].id[0]);
     });
 
-    return 'sss';
   });
 }
 
-function dispatch(title, xml) {
 
+const topics = {
+  '全般気象情報': 'weather-info',
+  '地方気象情報': 'weather-info',
+  '府県気象情報': 'weather-info',
+};
+
+function dispatch(id, title, xml) {
+  console.log(id, title, xml);
+  if (title in topics) {
+    const topic = topics[title];
+    console.log('->', topic);
+  }
 }
 
